65573792142b954f60ed252bdc5437c6a66022e3
Rishabh Kumar <rishabhkmr786@gmail.com>
2026-04-28 01:12:15 +0530

    date time utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,60 +2,99 @@ import capFirstLetter from './capFirstLetter';
 import capitalize from './capitalize';
 import camelCase from './camelCase';
 import chunk from './chunk';
+import clamp from './clamp';
 import compose from './compose';
 import deepClone from './deepClone';
 import flattenObject from './flattenObject';
+import formatCompactNumber from './formatCompactNumber';
+import formatCurrency from './formatCurrency';
+import formatDate from './formatDate';
+import formatNumber from './formatNumber';
+import formatTime from './formatTime';
 import generateInitials from './generateInitials';
+import getGreetingByTime from './getGreetingByTime';
 import groupBy from './groupBy';
 import hasEqualProps from './hasEqualProps';
+import addDays from './addDays';
+import countdown from './countdown';
+import differenceInDays from './differenceInDays';
 import isEmpty from './isEmpty';
 import isEqual from './isEqual';
 import isNonEmptyArray from './isNonEmptyArray';
+import isPast from './isPast';
+import isToday from './isToday';
 import kebabCase from './kebabCase';
 import keyBy from './keyBy';
 import maskEmail from './maskEmail';
 import maskPhone from './maskPhone';
 import mergeDeep from './mergeDeep';
 import omit from './omit';
+import percentage from './percentage';
 import pick from './pick';
+import randomBetween from './randomBetween';
 import randomInt from './randomInt';
 import randomValue from './randomValue';
 import removeExtraSpaces from './removeExtraSpaces';
+import roundTo from './roundTo';
 import slugify from './slugify';
 import snakeCase from './snakeCase';
 import sortBy from './sortBy';
+import timeAgo from './timeAgo';
 import titleCase from './titleCase';
 import truncate from './truncate';
 import uniqueBy from './uniqueBy';
 
 export default {
+  // string
   capFirstLetter,
   capitalize,
   camelCase,
+  generateInitials,
+  kebabCase,
+  maskEmail,
+  maskPhone,
+  removeExtraSpaces,
+  slugify,
+  snakeCase,
+  titleCase,
+  truncate,
+
+  // number
+  clamp,
+  formatCompactNumber,
+  formatCurrency,
+  formatNumber,
+  percentage,
+  randomBetween,
+  randomInt,
+  randomValue,
+  roundTo,
+
+  // date-time
+  addDays,
+  countdown,
+  differenceInDays,
+  formatDate,
+  formatTime,
+  getGreetingByTime,
+  isPast,
+  isToday,
+  timeAgo,
+
+  // object / array
   chunk,
   compose,
   deepClone,
   flattenObject,
-  generateInitials,
   groupBy,
   hasEqualProps,
   isEmpty,
   isEqual,
   isNonEmptyArray,
-  kebabCase,
   keyBy,
-  maskEmail,
-  maskPhone,
   mergeDeep,
   omit,
   pick,
-  randomInt,
-  randomValue,
-  removeExtraSpaces,
-  slugify,
-  snakeCase,
   sortBy,
-  titleCase,
-  truncate,
   uniqueBy,
 };
