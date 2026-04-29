8ef3adfc96264b95c7a03efca92875ad706ba3a1
Rishabh Kumar <rishabhkmr786@gmail.com>
2026-04-30 01:52:31 +0530

    fixed storybook main

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,3 @@
-import path from 'path';
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ['../stories/*.stories.jsx', '../stories/**/*.mdx'],
@@ -17,13 +16,7 @@ const config = {
         delete config.build.rollupOptions.output;
       }
     }
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...config.resolve?.alias,
-        'meticulous-ui': path.resolve(__dirname, '../src/index.js'),
-      },
-    };
+
     config.optimizeDeps = {
       ...config.optimizeDeps,
       include: [...(config.optimizeDeps?.include || []), 'styled-components'],
