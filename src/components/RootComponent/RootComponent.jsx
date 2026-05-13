750ffcbe03aa8d4be3f9137fb5dc20ca8260c6f9
Rishabh Kumar <rishabhkmr786@gmail.com>
2026-05-13 16:11:16 +0530

    RootComponent update

--- src/components/RootComponent/RootComponent.jsx
+++ src/components/RootComponent/RootComponent.jsx
@@ -1,12 +1,17 @@
 import PropTypes from 'prop-types';
+import { useMemo } from 'react';
 import styled from 'styled-components';
 
 const Root = styled.div`
-  font-size: 62.5%;
   -webkit-text-size-adjust: 100%;
 `;
 
 const RootComponent = ({ children, ...rest }) => {
+  useMemo(() => {
+    // Set 1rem = 10px for easier calculations throughout the app
+    document.documentElement.style.fontSize = '62.5%';
+  }, []);
+
   return <Root {...rest}>{children}</Root>;
 };
 
